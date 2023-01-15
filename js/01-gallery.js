e60b9cae62b6c82a549f4adcb7e7f8d118bd2fbf
Vitaly <nelyub86@ukr.net>
2023-01-15 18:16:24 +0200

    create render test #1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+
+const galleryCards = createGalleryItem(galleryItems);
+
+
+
+gallery.insertAdjacentHTML("beforeend", galleryCards);
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <div class="gallery__item">
+  <a class="gallery__link" href="${preview}">
+    <img
+      class="gallery__image"
+      src="${original}"
+      data-source="large-image.jpg"
+      alt="${description}"
+    />
+  </a>
+</div>
+  `;
+    })
+    .join("");
+}
