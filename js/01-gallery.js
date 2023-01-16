cdf0448ccbec68918e2d9bc013f9e6557f06d49f
Vitaly <nelyub86@ukr.net>
2023-01-16 20:31:40 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,10 +3,7 @@ import { galleryItems } from "./gallery-items.js";
 
 const gallery = document.querySelector(".gallery");
 
-
 const galleryCards = createGalleryItem(galleryItems);
-
-
 
 gallery.insertAdjacentHTML("beforeend", galleryCards);
 
@@ -15,11 +12,11 @@ function createGalleryItem(galleryItems) {
     .map(({ preview, original, description }) => {
       return `
   <div class="gallery__item">
-  <a class="gallery__link" href="${preview}">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
-      src="${original}"
-      data-source="large-image.jpg"
+      src="${preview}"
+      data-source="${original}"
       alt="${description}"
     />
   </a>
@@ -28,3 +25,27 @@ function createGalleryItem(galleryItems) {
     })
     .join("");
 }
+
+gallery.addEventListener("click", expandToFullScreen);
+
+function expandToFullScreen(event) {
+  event.preventDefault();
+  if (event.target.classList.contains("gallery__image")) {
+    console.log(event.target);
+  } else {
+    console.log("NO");
+  }
+}
+
+// import * as basicLightbox from "basiclightbox";
+
+// const instance = basicLightbox.create(`
+//     <div class="modal">
+//         <p>
+//             Your first lightbox with just a few lines of code.
+//             Yes, it's really that simple.
+//         </p>
+//     </div>
+// `);
+
+// instance.show();
